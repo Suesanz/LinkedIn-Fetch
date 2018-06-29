20e1862827bf4223b531456c00a54dba2b6c71ff
suesanz <yadavsourav24071998@gmail.com>
2018-06-30 01:38:57 +0530

    Updated
    Signed-off-by: suesanz <yadavsourav24071998@gmail.com>

--- server.js
+++ server.js
@@ -79,13 +79,13 @@ app.get('/profile', (req, res) => {
     ];
     res.json(customers);
 });
-// if (process.env.NODE_ENV === 'production') {
-//     // Set static folder
-//     app.use(express.static('client/build'));
-//
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//     });
-// }
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(config.PORT);
